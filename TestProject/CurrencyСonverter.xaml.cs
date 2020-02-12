d42cbdee04db741f2bca75e96db265e62856184f
PIBOP0116001 Тимченко Дмитрий Сергеевич <PIBOP0116001@sibupk.su>
2020-02-12 19:47:08 +0700

    Добавлена форма выбора валюты

--- TestProject/CurrencyСonverter.xaml.cs
+++ TestProject/CurrencyСonverter.xaml.cs
@@ -25,23 +25,35 @@ namespace TestProject
     /// </summary>
     public sealed partial class CurrencyСonverter : Page
     {
+        private string url = "https://www.cbr-xml-daily.ru/daily_json.js";
         public CurrencyСonverter()
         {
             this.InitializeComponent();
-            //создаем запрос к сайту по заданному URL
-            WebRequest webRequest = WebRequest.Create("https://www.cbr-xml-daily.ru/daily_json.js");
-            //если бы нужно было настроить параметры, это было бы здесь
-            //получаем ответ
+            WebRequest webRequest = WebRequest.Create(url);
             WebResponse webResponse = webRequest.GetResponse();
-            //создаем поток данных для чтения ответа
             Stream stream = webResponse.GetResponseStream();
             StreamReader streamReader = new StreamReader(stream);
-            //читаем html страничку в строку
             string result = streamReader.ReadToEnd();
             RootObject rootobject = JsonConvert.DeserializeObject<RootObject>(result);
 
             ChangeCurrency1.Content = string.Format("Изменить\nвалюту");
             ChangeCurrency2.Content = string.Format("Изменить\nвалюту");
+        }
+
+        private void ChangeCurrency1_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoForward)
+                Frame.GoForward();
+            else
+                Frame.Navigate(typeof(SelectCurrency));
+        }
+
+        private void ChangeCurrency2_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoForward)
+                Frame.GoForward();
+            else
+                Frame.Navigate(typeof(SelectCurrency));
         }
     }
 }
